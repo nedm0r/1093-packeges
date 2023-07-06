1595f8d7b92f036f9a8692930584e119bb5da410
nedm0r <134203412+nedm0r@users.noreply.github.com>
2023-07-06 14:33:00 +0300

    Update download-packeges-1093.sh

--- download-packeges-1093.sh
+++ download-packeges-1093.sh
@@ -6,31 +6,18 @@ package_file="packages.txt"
 readarray -t packages < "$package_file"
 
 # Install Ubuntu packages using apt-get
-for package in "${packages[@]}"
-do
-    if [[ $package == *"/"* ]]; then
-        sudo apt-get install -y "$package"
-    fi
-done
-
+# Clone Git repositories
 # Install Python packages using pip or pip3
 for package in "${packages[@]}"
 do
-    if [[ $package != *"/"* && $package != *"git clone"* ]]; then
-        sudo pip install "$package"  # Use 'pip3' if required
-    fi
-done
-
-# Install packages installed via git clone
-for package in "${packages[@]}"
-do
-    if [[ $package == *"git clone"* ]]; then
-        package_url="${package#*git clone }"
-        package_name="$(basename "$package_url" .git)"
-        git clone "$package_url"
-        cd "$package_name" || continue
-        sudo python setup.py install
-        cd ..
-        rm -rf "$package_name"
+    if [[ $package == *"/"* ]]; then
+        sanitized_package=$(echo "$package" | cut -d'/' -f1)  # Extract package name before the '/'
+        sudo apt-get install -y "$sanitized_package"
+    elif [[ $package == "git+"* ]]; then
+        repository=$(echo "$package" | cut -d'+' -f2)  # Extract repository URL after 'git+'
+        git clone "$repository"
+    else
+        sanitized_package=$(echo "$package" | tr -d '\r')  # Remove carriage return if present
+        sudo pip install "$sanitized_package"  # Use 'pip3' if required
     fi
 done
